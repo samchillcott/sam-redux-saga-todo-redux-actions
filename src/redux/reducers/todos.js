e38deab0a2c0a11d8ac6fdee86f81a9b7fcd2bd2
Sam Chillcott <samchillcott@gmail.com>
2021-01-02 12:44:56 +0000

    useDownloadTodos

--- src/redux/reducers/todos.js
+++ src/redux/reducers/todos.js
@@ -1,93 +1,60 @@
 import { handleActions } from 'redux-actions';
 import { v4 as uuidv4 } from 'uuid';
 import { saveTodos } from '../../api';
-import { addTodo, completeTodo, removeTodo } from '../actions';
+import { addTodo, completeTodo, editTodo, removeTodo, useDownloadTodos } from '../actions';
 
 const initialState = [];
 let newState;
 
-export const reducer = handleActions(
-    {
-        [addTodo]: (
-            todos, { payload: text }
-        ) => {
-            newState = [...todos, 
-                {isComplete: false,
-                key: uuidv4(),
-                text: text
-                }];
-            saveTodos(newState);
-            return newState;
-        },
-        [completeTodo]: (
-            todos, action
+export const reducer = handleActions({
+    [addTodo]: (
+        todos, { payload: text }
+    ) => {
+        newState = [...todos, 
+            {isComplete: false,
+            key: uuidv4(),
+            text: text
+            }];
+        saveTodos(newState);
+        return newState;
+    },
+    [completeTodo]: (
+        todos, action
+    ) => {
+        newState = todos.map(todo => {
+            if (todo.key === action.payload.key) {
+                action.payload.isComplete = !action.payload.isComplete
+                return action.payload   
+            } else {
+                return todo
+            }
+        });
+        saveTodos(newState);
+        return newState
+    },
+    [removeTodo]: (
+        todos, action
+    ) => {
+        newState = todos.filter(todo => todo.key !== action.payload.key)
+        saveTodos(newState);
+        return newState
+    },
+    [editTodo]: (
+        todos, action
         ) => {
             newState = todos.map(todo => {
-               if (todo.key === action.payload.key) {
-                    action.payload.isComplete = !action.payload.isComplete
-                    return action.payload   
-               } else {
-                   return todo
-               }
-            });
-            saveTodos(newState);
-            return newState
+            if (todo.key === action.payload.key) {
+                return action.payload
+            } else {
+                return todo
+            }
+        });
+        saveTodos(newState);
+        return newState
         },
-        [removeTodo]: (
-            todos, action
-        ) => {
-            newState = todos.filter(todo => todo.key !== action.payload.key)
-            saveTodos(newState);
-            return newState
-        }
-    }, 
-    initialState
-);
-
-// const todoReducer = (todos = initialState, action) => {
-//     switch (action.type) {
-//         case 'ADD_TODO':
-//             newState = [...todos, 
-//                 {isComplete: false,
-//                 key: uuidv4(),
-//                 text: action.payload
-//                 }];
-//             saveTodos(newState);
-//             return newState
-//         case 'COMPLETE_TODO':
-//             console.log({payload: action.payload},"completeTodo from reducer");
-//             newState = todos.map(todo => {
-//                if (todo.key === action.payload.key) {
-//                     action.payload.isComplete = !action.payload.isComplete
-//                     return action.payload   
-//                } else {
-//                    return todo
-//                }
-//             });
-//             saveTodos(newState);
-//             return newState
-//         case 'REMOVE_TODO':
-//             console.log({payload: action.payload},"removeTodo fired from reducer");
-//             newState = todos.filter(todo => todo.key !== action.payload.key)
-//             saveTodos(newState);
-//             return newState
-//         case 'EDIT_TODO':
-//             console.log({payload: action.payload},"edit fired from reducer");
-//             newState = todos.map(todo => {
-//                if (todo.key === action.payload.key) {
-//                    return action.payload
-//                } else {
-//                    return todo
-//                }
-//             });
-//             saveTodos(newState);
-//             return newState
-//         case 'USE_DOWNLOAD_TODOS':
-//             console.log({payload: action.payload},"download fired from reducer");
-//             return action.payload
-//         default:
-//             return todos
-//     }
-// }
-
-// export default todoReducer;
+    [useDownloadTodos]: (
+        action
+    ) => {
+        return action.payload
+    }
+}, initialState);
